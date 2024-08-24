8e62a2f84d5b113d2e070f173f5a4b0edeaca703
Jonas Kett <jonas.kett13@gmail.com>
2024-08-24 15:23:45 +0200

    better docs

--- ImageSorter/Program.cs
+++ ImageSorter/Program.cs
@@ -1,27 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-using System.CommandLine;
+using  System.CommandLine;
 using ImageSorter;
 using ImageSorter.Services.DateParser;
 using ImageSorter.Services.FileHandling;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-var rootCommand = new RootCommand(description: "sorts images based on the taken date from their meta data");
+var description = @"Sorts images chronologically in the directory structure year/month
+Will use the following clues to determine a date for each file in order:
+
+- Exif Tag DateTimeOriginal  (0x9003)
+- Exif Tag DateTimeDigitized (0x9004)
+- Exif Tag DateTime          (0x0132)
+- Regex for file name: .*(20[0-9]{2}|19[0-9]{2})-(0[0-9]|1[0-9])-(0[0-9]|1[0-9]|2[0-9]|3[0-1]).* : detects yyyy-mm-dd
+- Regex for file name: .*(20[0-9]{2}|19[0-9]{2})(0[0-9]|1[0-9])(0[0-9]|1[0-9]|2[0-9]|3[0-1]).*   : detects yyyymmdd
+- Last edit date from file system
+";
+
+var rootCommand = new RootCommand(description: description);
 var sourceArgument = new Argument<FileInfo>("source path", "The path of the source directory");
-var destinationOption = new Option<FileInfo>(aliases: new[] { "--dest" }, description: "The path of the destination directory (required if not --in-place)");
-var inPlaceOption = new Option<bool>(aliases: new[] { "--move" }, description: "[EXPERIMENTAL] Move files instead of copy", getDefaultValue: () => false);
+var destinationOption = new Option<FileInfo?>(aliases: new[] { "--dest" },
+    description: "The path of the destination directory (required if not --move)");
+var inPlaceOption = new Option<bool>(aliases: new[] { "--move" }, description: "Move files instead of copy",
+    getDefaultValue: () => false);
 var fileEndingsOption = new Option<string[]>(aliases: new[] { "--types", "-t" },
-    description: "space seperated list of file endings to copy");
+    description: "Space seperated list of file endings to sort");
 var overwriteOption =
-    new Option<bool>(aliases: new[] { "--overwrite" }, description: "Overwrite files in destination", getDefaultValue: () => false);
+    new Option<bool>(aliases: new[] { "--overwrite" }, description: "Overwrite files in destination",
+        getDefaultValue: () => false);
 var parallelScanningOption =
-    new Option<bool>(aliases: new[] { "--scan-parallel" }, description: "Perform the scan part in parallel", getDefaultValue: () => false);
-var fromOption = new Option<DateTime?>(aliases: new[] { "--from" }, description: "min date for files to sort");
-var toOption = new Option<DateTime?>(aliases: new[] { "--to" }, description: "max date for files to sort");
+    new Option<bool>(aliases: new[] { "--scan-parallel" }, description: "Perform the scan part in parallel",
+        getDefaultValue: () => false);
+var fromOption = new Option<DateTime?>(aliases: new[] { "--from" }, description: "Minimum date for files to sort");
+var toOption = new Option<DateTime?>(aliases: new[] { "--to" }, description: "Maximum date for files to sort");
 var progressOption = new Option<int?>(aliases: new[] { "--progress-at" },
-    description: "written file count after which a progress update is printed", getDefaultValue: () => 1000);
+    description: "Processed file count after which a progress update is printed", getDefaultValue: () => 1000);
 rootCommand.AddArgument(sourceArgument);
 rootCommand.AddOption(destinationOption);
 rootCommand.AddOption(inPlaceOption);
@@ -43,7 +58,7 @@ rootCommand.SetHandler(async (context) =>
     {
         throw new ArgumentException("no destination path provided but in-place was not set");
     }
-    
+
     var runConfig = new RunConfiguration
     {
         SourcePath = parsedContext.GetValueForArgument(sourceArgument),
--- Readme.md
+++ Readme.md
@@ -2,11 +2,43 @@
 
 A simply utility to sort a bunch of images chronologically.
 
-It uses the tags on the image, the file name and finally the file edit date to determine the date on which the image was created.
-
 ## How to use
 
-``dotnet run --project ImageSorter`` will provide the help
+You need the [.NET 8.0 Runtime](https://dotnet.microsoft.com/en-us/download/dotnet/8.0) to use it.
+
+Run ``dotnet run --project ImageSorter -- --help`` to get the help:
+
+````
+Description:
+  Sorts images chronologically in the directory structure year/month
+  Will use the following clues to determine a date for each file in order:
+
+  - Exif Tag DateTimeOriginal  (0x9003)
+  - Exif Tag DateTimeDigitized (0x9004)
+  - Exif Tag DateTime          (0x0132)
+  - Regex for file name: .*(20[0-9]{2}|19[0-9]{2})-(0[0-9]|1[0-9])-(0[0-9]|1[0-9]|2[0-9]|3[0-1]).* : detects yyyy-mm-dd
+  - Regex for file name: .*(20[0-9]{2}|19[0-9]{2})(0[0-9]|1[0-9])(0[0-9]|1[0-9]|2[0-9]|3[0-1]).*   : detects yyyymmdd
+  - Last edit date from file system
+
+
+Usage:
+  ImageSorter <source path> [options]
+
+Arguments:
+  <source path>  The path of the source directory
+
+Options:
+  --dest <dest>                The path of the destination directory (required if not --move)
+  --move                       Move files instead of copy [default: False]
+  -t, --types <types>          Space seperated list of file endings to sort
+  --overwrite                  Overwrite files in destination [default: False]
+  --from <from>                Minimum date for files to sort
+  --to <to>                    Maximum date for files to sort
+  --scan-parallel              Perform the scan part in parallel [default: False]
+  --progress-at <progress-at>  Processed file count after which a progress update is printed [default: 1000]
+  --version                    Show version information
+  -?, -h, --help               Show help and usage information
+````
 
 ## TODOs
 
