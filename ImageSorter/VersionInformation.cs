65d1a70d92826fc8e06e96fdc29167d0ead24a30
Jonas Kett <jonas.kett13@gmail.com>
2024-11-23 11:38:15 +0100

    updated version information

--- ImageSorter/VersionInformation.cs
+++ ImageSorter/VersionInformation.cs
@@ -5,10 +5,10 @@ public static class VersionInformation
     /// <summary>
     /// Version string which matches release tag in git
     /// </summary>
-    public const string Version = "v0.1.1";
+    public const string Version = "v0.1.2";
 
     /// <summary>
     /// Configured for the assembly in the <c>ImageSorter.csproj</c>
     /// </summary>
-    public const string AssemblyVersion = "0.1.1.0";
+    public const string AssemblyVersion = "0.1.2.0";
 }
