c326a8cfba0f66a63049c64ccb7cc4ea1423655a
Jonas Kett <jonas.kett@exxeta.com>
2025-09-12 18:08:56 +0200

    fixed bug in CalculateBinomialCoefficient and added unit tests for it

---
+++ ImageSorter.Utils.Tests/BinomialCoefficientTests.cs
@@ -0,0 +1,24 @@
+﻿using ImageParser.Utils.Math;
+
+namespace ImageSorter.Utils.Tests;
+
+[TestFixture]
+public class BinomialCoefficientTests
+{
+    [TestCase(0, 0, ExpectedResult = 1)]
+    [TestCase(1, 0, ExpectedResult = 1)]
+    [TestCase(1, 1, ExpectedResult = 1)]
+    [TestCase(2, 0, ExpectedResult = 1)]
+    [TestCase(2, 1, ExpectedResult = 2)]
+    [TestCase(2, 2, ExpectedResult = 1)]
+    [TestCase(3, 0, ExpectedResult = 1)]
+    [TestCase(3, 1, ExpectedResult = 3)]
+    [TestCase(3, 2, ExpectedResult = 3)]
+    [TestCase(3, 3, ExpectedResult = 1)]
+    [TestCase(49,6, ExpectedResult = 13_983_816)]
+    [TestCase(0, 1, ExpectedResult = 0)]
+    public long CalculateBinomialCoefficient(long n, long k)
+    {
+        return BinomialCoefficient.CalculateBinomialCoefficient(n, k);
+    }
+}
--- ImageSorter.Utils/Math/BinomialCoefficient.cs
+++ ImageSorter.Utils/Math/BinomialCoefficient.cs
@@ -2,23 +2,23 @@ namespace ImageParser.Utils.Math;
 
 public static class BinomialCoefficient
 {
+    /// <summary>
+    /// Calculates the <see href="https://en.wikipedia.org/wiki/Binomial_coefficient">Binomial Coefficient</see>
+    /// </summary>
+    /// <remarks>
+    /// The binomial coefficient is defined as <c>N! / ( K! (N - K)! )</c>.
+    /// </remarks>
     public static long CalculateBinomialCoefficient(long n, long k)
     {
-        // This function gets the total number of unique combinations based upon N and K.
-        // N is the total number of items.
-        // K is the size of the group.
-        // Total number of unique combinations = N! / ( K! (N - K)! ).
-        // This function is less efficient, but is more likely to not overflow when N and K are large.
-        // Taken from:  http://blog.plover.com/math/choose.html
-        //
-        long r = 1;
-        long d;
-        if (n > k) return 0;
-        for (d = 1; d <= k; d++)
+        // This implementation uses the multiplicative formula since it is less likely to overflow for large values:
+        // see https://en.wikipedia.org/wiki/Binomial_coefficient#Multiplicative_formula
+        if (k > n) return 0;
+        long result = 1;
+        for (long i = 1; i <= k; i++)
         {
-            r *= n--;
-            r /= d;
+            result *= n--;
+            result /= i;
         }
-        return r;
+        return result;
     }
 }
