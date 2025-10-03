2d8cc9325a759335652095734b55547b3a8d40f9
Jonas Kett <jonas.kett13@gmail.com>
2025-10-03 16:50:46 +0200

    removed unused code

--- ImageSorter.Services/FileHandling/DateDirectoryOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace ImageSorter.Services.FileHandling;
-
-public class DateDirectoryOptions
-{
-    public required string? Format { get; set; }
-    
-    public required string DestinationPath { get; set; }
-}
--- ImageSorter.Services/FileHandling/FileHandlingServiceCollectionExtension.cs
+++ ImageSorter.Services/FileHandling/FileHandlingServiceCollectionExtension.cs
@@ -1,30 +1,9 @@
-using ImageSorter.FileHandling.Directory;
-using ImageSorter.FileHandling.File;
-using ImageSorter.FileHandling.FileStream;
-using ImageSorter.FileHandling.Path;
-using ImageSorter.FileWrapper.Abstractions.Directory;
-using ImageSorter.FileWrapper.Abstractions.File;
-using ImageSorter.FileWrapper.Abstractions.FileStream;
-using ImageSorter.FileWrapper.Abstractions.Path;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ImageSorter.Services.FileHandling;
 
 public static class FileHandlingServiceCollectionExtension
 {
-    public static IServiceCollection AddDestinationWriter(
-        this IServiceCollection serviceCollection,
-        DestinationWriterOptions options,
-        DateDirectoryOptions dateDirectoryOptions)
-    {
-        serviceCollection.AddSingleton(dateDirectoryOptions);
-        serviceCollection.AddSingleton<IDateDirectory, ConfigurableDateDirectory>();
-        serviceCollection.AddSingleton(options);
-        serviceCollection.AddSingleton<IDestinationWriter, DestinationWriter>();
-
-        return serviceCollection;
-    }
-
     public static IServiceCollection AddFileLoader(
         this IServiceCollection serviceCollection,
         FileLoaderOptions options,
