ba9f3771c234ccae8a5d0198e0fd934595e7a06c
Jonas Kett <jonas.kett13@gmail.com>
2025-10-03 16:37:55 +0200

    removed cursor console stuff

--- ImageSorter.Console/Program.cs
+++ ImageSorter.Console/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System.CommandLine;
 using ImageSorter;
 
-Console.CursorVisible = false;
 var rootCommand = RootCommandFactory.BuildRootCommand();
 rootCommand.SetHandler(RootCommandHandler.Handle);
-await rootCommand.InvokeAsync(args);
+try
+{
+    // TODO to error handling (e.g. catch some exceptions and print a semi pretty error message)
 
-// TODO to error handling (e.g. catch some exceptions and print a semi pretty error message)
-
-// here so all log statements make it to the console
-Thread.Sleep(50);
+    await rootCommand.InvokeAsync(args);
+}
+finally
+{
+    // here so all log statements make it to the console
+    Thread.Sleep(50);
+}
