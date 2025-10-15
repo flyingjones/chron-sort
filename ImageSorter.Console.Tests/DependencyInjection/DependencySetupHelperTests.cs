437602a833945608a3c79d8b71a1013f6a7bdee7
Jonas Kett <jonas.kett@exxeta.com>
2025-10-15 07:47:29 +0200

    added DI Tests

---
+++ ImageSorter.Console.Tests/DependencyInjection/DependencySetupHelperTests.cs
@@ -0,0 +1,87 @@
+﻿using ImageSorter.DependencyInjection;
+using ImageSorter.Markdown.Abstractions.Services;
+using ImageSorter.Markdown.Services;
+using ImageSorter.Services;
+using ImageSorter.Sorting.Model;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ImageSorter.Tests.DependencyInjection;
+
+[TestFixture]
+public class DependencySetupHelperTests
+{
+    [TestCaseSource(nameof(GetTestCases))]
+    public void SetupServices_NoMissingDependencies(DependencySetupHelperTestCase testCase)
+    {
+        // arrange
+        var serviceCollection = testCase.ToRunConfiguration().SetupServices();
+        // initialized in the main and not the normal service stuff
+        serviceCollection.AddSingleton<IMarkdownFileWriter, MockMarkdownFileWriter>();
+        
+        // act & assert
+        Assert.DoesNotThrow(() =>
+        {
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            
+            // top level dependencies which are constructed in the main
+            var logger = serviceProvider.GetRequiredService<ILogger<DependencySetupHelperTests>>();
+            var markDownRenderEngine = serviceProvider.GetRequiredService<IMarkdownTableRenderEngine>();
+            var sorter = serviceProvider.GetRequiredService<ISorter>();
+        });
+    }
+    
+    private static IEnumerable<DependencySetupHelperTestCase> GetTestCases()
+    {
+        yield return new DependencySetupHelperTestCase
+        {
+            IsDryRun = false,
+            MoveFiles = false,
+            ScanParallel = false,
+            UseProgressBar = false,
+            DestinationConflictMode = DestinationConflictMode.Skip,
+            ConflictReducerMode = ConflictReducerMode.None,
+            ConflictResolverMode = ConflictResolverMode.Throw
+        };
+        yield return new DependencySetupHelperTestCase
+        {
+            IsDryRun = true,
+            MoveFiles = true,
+            ScanParallel = true,
+            UseProgressBar = true,
+            DestinationConflictMode = DestinationConflictMode.Overwrite,
+            ConflictReducerMode = ConflictReducerMode.FileLength,
+            ConflictResolverMode = ConflictResolverMode.ChooseOne
+        };
+        yield return new DependencySetupHelperTestCase
+        {
+            IsDryRun = false,
+            MoveFiles = false,
+            ScanParallel = false,
+            UseProgressBar = true,
+            DestinationConflictMode = DestinationConflictMode.Joint,
+            ConflictReducerMode = ConflictReducerMode.FileContent,
+            ConflictResolverMode = ConflictResolverMode.RandomRename
+        };
+        yield return new DependencySetupHelperTestCase
+        {
+            IsDryRun = false,
+            MoveFiles = false,
+            ScanParallel = false,
+            UseProgressBar = true,
+            DestinationConflictMode = DestinationConflictMode.Joint,
+            ConflictReducerMode = ConflictReducerMode.FileContent,
+            ConflictResolverMode = ConflictResolverMode.HashRename
+        };
+        yield return new DependencySetupHelperTestCase
+        {
+            IsDryRun = false,
+            MoveFiles = false,
+            ScanParallel = false,
+            UseProgressBar = true,
+            DestinationConflictMode = DestinationConflictMode.Joint,
+            ConflictReducerMode = ConflictReducerMode.FileContent,
+            ConflictResolverMode = ConflictResolverMode.SemanticRename
+        };
+    }
+}
--- ImageSorter.Console/DependencyInjection/DependencySetupHelper.cs
+++ ImageSorter.Console/DependencyInjection/DependencySetupHelper.cs
@@ -25,6 +25,8 @@ public static class DependencySetupHelper
 {
     public static IServiceCollection SetupServices(this RunConfiguration configuration)
     {
+        PreprocessRunConfiguration(configuration);
+        
         var serviceCollection = new ServiceCollection();
         serviceCollection.AddTransient<IDateTimeProvider, DateTimeProvider>();
         serviceCollection.AddTransient<IRandomStringGenerator>(_ => RandomStringGenerator.CreateForLowerCaseLetters());
@@ -40,36 +42,9 @@ public static class DependencySetupHelper
         });
         serviceCollection.AddStopwatchLogger(configuration.LogLevel);
         serviceCollection.AddSingleton<ILazyFileMetaDataHandleFactory, LazyFileMetaDataHandleFactory>();
-        if (configuration.UseDefaultSortConfiguration)
-        {
-            configuration.SortConfiguration = SortConfigurationFactory.DefaultSorting.ToArray();
-        }
-
-        if (configuration.PreferFileNameParsing)
-        {
-            configuration.SortConfiguration = configuration.SortConfiguration!
-                .GroupBy(x => x.Split(':', 2)[0], x => x)
-                .OrderBy(x => x.Key == $"{SortType.FileName:G}" ? 0 : 1)
-                .SelectMany(x => x)
-                .ToArray();
-        }
 
         serviceCollection.ConfigureSorting(configuration.SortConfiguration!);
 
-        // serviceCollection.AddDestinationWriter(new DestinationWriterOptions
-        //     {
-        //         SourcePath = configuration.SourcePath.FullName,
-        //         DestinationPath = configuration.DestinationPath.FullName,
-        //         OverwriteExistingFiles = configuration.DestinationConflictMode == DestinationConflictMode.Overwrite,
-        //         From = configuration.From,
-        //         To = configuration.To,
-        //         ProgressCount = configuration.ProgressAt > 0 ? configuration.ProgressAt.Value : int.MaxValue
-        //     },
-        //     new DateDirectoryOptions
-        //     {
-        //         DestinationPath = configuration.DestinationPath.FullName,
-        //         Format = configuration.OutputFormat,
-        //     });
         serviceCollection.AddSingleton(
             new ResultWriterConfig
             {
@@ -140,5 +115,22 @@ public static class DependencySetupHelper
             configuration.FileSystemIsCaseSensitive);
 
         return serviceCollection;
+    }
+
+    private static void PreprocessRunConfiguration(RunConfiguration configuration)
+    {
+        if (configuration.UseDefaultSortConfiguration)
+        {
+            configuration.SortConfiguration = SortConfigurationFactory.DefaultSorting.ToArray();
+        }
+
+        if (configuration.PreferFileNameParsing)
+        {
+            configuration.SortConfiguration = configuration.SortConfiguration!
+                .GroupBy(x => x.Split(':', 2)[0], x => x)
+                .OrderBy(x => x.Key == $"{SortType.FileName:G}" ? 0 : 1)
+                .SelectMany(x => x)
+                .ToArray();
+        }
     }
 }
